53fb9fc0b5002b3d1c6970e2366293e5af486041
MrHaan <49995469+MrHaan@users.noreply.github.com>
2022-12-15 14:31:11 +0100

    move test code to dummy

--- simulator.m
+++
@@ -1,9 +0,0 @@
-pv = PV("data/solarPanelOutputDataSlimPark.csv");
-t= timeofday(datetime("today"))+ seconds(3600*7)
-pv.advance_time_to(t);
-P = pv.P
-
-driveev = DriveEV("data/20221206GS_DEMSdata_spikyPV.xlsx");
-evs = [0;1;54]
-driveev.select_EVs(evs);
-arrived_evs = driveev.advance_time_to(t)
