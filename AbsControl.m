4a0c2cd80de922767055508e640b11d14df6c619
MrHaan <49995469+MrHaan@users.noreply.github.com>
2022-12-20 21:09:34 +0100

    rename

---
+++ AbsControl.m
@@ -0,0 +1,22 @@
+function [Pcharger] = AbsControl(Ptarget,Ptrafo,Pprev, N)
+%AbsControl Controlling the chargers in absolute steps
+%Input:
+%	Ptarget	= the wanted value for Ptrafo
+%	Ptrafo	= the power at the trafo (power consumed by EV - power produced by PV)
+%	Pprev	= previous value of Pcharger
+%	N		= the number of chargers
+%Output:
+%	Pcharger = The power for each charger. This is an N-length vector, because
+%	in FCFS chargers will get different power.
+deadband = 0.05; % dead band in percent
+Prest = (1-deadband) * Ptarget; % restoration power limit
+
+if (Ptrafo <= Ptarget) && (Ptrafo > Ptarget * Prest)
+	% Prevents the Pcharger from constantly updating with only small
+	% changes
+	Pcharger = Pprev;
+else
+	% If all chargers will use Pev than the Ptarget will be reached 
+	Pcharger = Pprev + ones([1 N])*(Ptarget - Ptrafo)/N;		
+end
+end
