de2a7afeb997e12cb2c6ca41ad5b5f4200df08e7
MrHaan <49995469+MrHaan@users.noreply.github.com>
2023-01-10 14:47:27 +0100

    simplify input table to matrix

--- FCFSController.m
+++ FCFSController.m
@@ -15,65 +15,54 @@ classdef FCFSController < handle
 		   obj.Pcharge_max = Pcharge_max;
         end
 
-        function [Pchargers,delta] = update(obj, Ptrafo, charger_readings)
+        function [Pchargers,delta] = update(obj, Ptrafo, Pchargers)
             % update: update the FCFS controller state.
             % Input:
             %   Ptrafo: current power of the transformer in W
-            %   charger_readings: table with the following columns:
-            %       id: identifier of the charger
-            %       arrival_time: arrival time of the EV, nan if not
-            %       applicable
-            %       p: current charging power in W
+            %   Pchargers: array of charging power for each charger
+            %       connected to an EV. NOTE: This assumes that the array
+            %       is sorted such that the most recently connected EV is
+            %       the last element and the oldest EV is the first element.
             % Output:
-            %   Pchargers: table with columns: 
-            %       id: identifier of the charger
-            %       p: updated of the power for each charger, based on
-            %       the control algorithm
+            %   Pchargers: array of charging power for each charger
+            %       connected to an EV.
 
             % Update step
 			delta = Ptrafo - obj.Pmax;
 
-            % Sort the chargers, most recently connected EV in first row
-            % Note: NaN goes to the last entries, which is desired
-            charger_readings = sortrows(charger_readings, "arrival_time", "descend");
-
             % Update charger power 
 			% (ATM only possible to give chargers less power and not more)
 			if delta > 0		% The trafo is going over limit, so we should charge less 
-				for row = 1:size(charger_readings,2)           
-                	if charger_readings.p(row) >= obj.Pcharge_min + delta
-                    	charger_readings.p(row) = charger_readings.p(row) - delta;
+				for i = 1:numel(Pchargers)        
+                	if Pchargers(i) >= obj.Pcharge_min + delta
+                    	Pchargers(i) = Pchargers(i) - delta;
                     	delta = 0;
                     	break;
 					else
-						delta = delta - (charger_readings.p(row) - obj.Pcharge_min);	%code breaks when I swap this line with one below
-                    	charger_readings.p(row) = obj.Pcharge_min;
+						delta = delta - (Pchargers(i) - obj.Pcharge_min);	%code breaks when I swap this line with one below
+                    	Pchargers(i) = obj.Pcharge_min;
                 	end
 				end
-				for row = 1:size(charger_readings,2)	%lower to zero if lowering to minimum is not enough 
+				for i = 1:numel(Pchargers)	%lower to zero if lowering to minimum is not enough 
                 	if delta < 0
 						break
 					else
-						delta = delta - charger_readings.p(row);	%code breaks when I swap this line with one below
-                    	charger_readings.p(row) = 0;
+						delta = delta - Pchargers(i);	%code breaks when I swap this line with one below
+                    	Pchargers(i) = 0;
                 	end
 				end
 			elseif delta < 0	% The EVs can be charged with more power
-				for row = size(charger_readings,2):-1:1    % The EV that arrived first gets it P increased first       
-                	if charger_readings.p(row) <=  delta + obj.Pcharge_max
-                    	charger_readings.p(row) = charger_readings.p(row) - delta;
+				for i = numel(Pchargers):-1:1    % The EV that arrived first gets it P increased first       
+                	if Pchargers(i) <=  delta + obj.Pcharge_max
+                    	Pchargers(i) = Pchargers(i) - delta;
                     	delta = 0;
                     	break;
 					else
-						delta = delta + (obj.Pcharge_max - charger_readings.p(row));	%code breaks when I swap this line with one below
-                    	charger_readings.p(row) = obj.Pcharge_max;
+						delta = delta + (obj.Pcharge_max - Pchargers(i));	%code breaks when I swap this line with one below
+                    	Pchargers(i) = obj.Pcharge_max;
                 	end
 				end
-			end
-
-            Pchargers = table;
-            Pchargers.id = charger_readings.id;
-            Pchargers.p = charger_readings.p;
+            end
         end
     end
 end
