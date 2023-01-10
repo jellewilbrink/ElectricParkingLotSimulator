1a23d56e34426a1fcd54ea857b8b023b52a6aaad
s2163047 <d.h.matena@student.utwente.nl>
2023-01-10 14:04:01 +0100

    Create Transformer.m

---
+++ Transformer.m
@@ -0,0 +1,20 @@
+classdef Transformer < handle
+    %UNTITLED Summary of this class goes here
+    %   This class represents the transformer with all of its variables 
+    %   and functions 
+    
+    properties
+        pmax {mustBeNumeric}
+        power {mustBeNumeric}
+    end
+    
+    methods
+        function obj = Transformer(pmax, power)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.pmax = pmax;
+            obj.power = power;
+        end
+    end
+end
+
